d97870469f5e2759434f3c478d46efb08b64401e
jcarron <to.jcarron@gmail.com>
2024-01-26 16:12:57 +0100

    misc

--- 20240124_chwide-noise/chwide.py
+++ 20240124_chwide-noise/chwide.py
@@ -2,11 +2,21 @@ import glob
 import os
 import healpy as hp
 import numpy as np
+import numpy.random
+
 from lenspyx.utils_hp import Alm, almxfl, gauss_beam
 from lenspyx.remapping.utils_geom import Geom
+from lenspyx.utils import cli
 from scipy.interpolate import UnivariateSpline as spl
-from multiprocessing import cpu_count
 from numpy.random import default_rng
+from psutil import cpu_count
+
+from lenspyx.utils import timer
+from lenspyx import cachers
+
+UNSEEN = -1.6375e+30
+assert hp.UNSEEN == UNSEEN
+pix_min, npix = 14135296, 33839732
 
 # --- freqs on disk for alt1 chile wide ---
 freqs = [30, 40, 90, 150, 220, 280]
@@ -23,6 +33,7 @@ else: # Most likely we are on NERSC
 
 nside = 128
 tht_min, tht_max = (64 / 180 * np.pi, 155. / 180 * np.pi)
+zbounds = np.cos([tht_max, tht_min])
 
 # --- PBDR 1/f noise parameters ---
 freqs_PBDR = [25, 40, 90, 150, 230, 280] # (! not the same as DC0)
@@ -35,8 +46,8 @@ nlev_P_PBDR = [37.6, 15.5, 2.7, 3.0, 9.8, 23.9]
 beam_PBDR = [7.8, 5.3, 2.2, 1.4, 1.0, 0.9]
 
 def get_lknee_a(TorP, freq):
-    lknee, a = (lknee_T_PBDR, a_T) if TorP == 'T' else (lknee_P_PBDR, a_P)
-    return spl(freqs_PBDR, lknee, k=1, s=0)(freq),spl(freqs_PBDR, a, k=1, s=0)(freq)
+    lknee, alph = (lknee_T_PBDR, a_T) if TorP.upper() in ['T', 'I'] else (lknee_P_PBDR, a_P)
+    return spl(freqs_PBDR, lknee, k=1, s=0)(freq),spl(freqs_PBDR, alph, k=1, s=0)(freq)
 
 def get_nlev_ukamin(freq: int, xx: str, thres=10, nside_out=128):
     """Get's noise level from Cl_noise = mean (pix var map)
@@ -49,7 +60,6 @@ def get_nlev_ukamin(freq: int, xx: str, thres=10, nside_out=128):
     cov =  hp.read_map(path, field=XYs.index(xx.upper()))
     assert cov.size == 12 * nside ** 2, (cov.size, 12 * nside ** 2)
     if nside_out != nside:
-        #FIXME: rather an map2alm and alm2map, to avoid features on the ig pixels boundaries ?
         cov = hp.ud_grade(cov, nside_out) * (nside_out / nside) ** 2
     cov = cov[np.where(cov > 0)]
     nlev = np.sqrt(np.mean(cov[np.where(cov < thres ** 2 * np.min(cov))]))
@@ -73,8 +83,13 @@ def get_mask(xx:str, nside_mask=2048, freqs2consider=(90, 150), thres=10.):
         mask *= (cov > 0) & (cov <= thres ** 2 * covmin)
     return mask
 
+
 def get_cov(freq: int, xx:str, nside_out=2048):
-    assert xx.upper() in ['II', 'QQ', 'UU', 'QU']
+    """Reijo' CMB covariance possibly upgraded to larger nside
+
+
+    """
+    assert xx.upper() in ['II', 'QQ', 'UU', 'QU', 'TT']
     assert freq in freqs, (freq, freqs)
     path = path2cov%freq
     cov =  hp.read_map(path, field=XYs.index(xx.upper())) * 1e12 # in uK^2
@@ -105,106 +120,87 @@ def _syn_alm(rng, cl:np.ndarray):
     alm[real_idcs] = alm[real_idcs].real * np.sqrt(2.)
     return alm
 
-def get_sim_map_v2(freq, lmax_atm=5120, nside_out=2048, seed=None, _facknee=1., _facwhite=1.):
+def build_sims(freq, mcs:np.ndarray[int], rng:np.random.Generator, cache:cachers.cacher=cachers.cacher_mem(),
+                   lmax_atm=5120, nside_out=2048, _facknee=1., _facwhite=1.):
     """
-    
+
         Args:
             freq: CHLAT frequency
+            mcs: indices of sim to be built
+            rng: random number generator
+            cache: cacher (cacher_npy for example)
             lmax_atm: Gaussian atm. noise simulated down to this multipole
             nside_out: nside of the output map
-            seed: input simulation seed
+            _facwhite: rescales the white noise sim with this factor
+            _facknee: rescales the atm part with this factor
 
         Note:
-            white noise perfecly independent from pixel to pixel
+            white noise perfecly independent pixel to pixel
             Gaussian atm noise with equal E and B power, rescaled at the map level to match Reijo's depth at the lknee
             Include QU white noise covariance
+            Features on the noise maps owing to covariance upgraded from 128 to nside_out
 
     """
+    tim = timer('simgen')
+    tim.start('one-off')
     # ---- sim params and geom ----
     mmax = lmax_atm
     lknee_I, alpha_I = get_lknee_a('T', freq)
     lknee_P, alpha_P = get_lknee_a('P', freq)
 
-    cl_white = np.ones(lmax_atm + 1, dtype=float)
-    s2_white_I = np.sum(cl_white * (2 * np.arange(lmax_atm + 1) + 1)) / (4 * np.pi)
-    s2_white_P = np.sum(cl_white[2:] * (2 * np.arange(2, lmax_atm + 1) + 1)) / (4 * np.pi)  # ( EE + BB)
 
     s_II = np.sqrt(get_cov(freq, 'II', nside_out=nside_out))
     s_QQ = np.sqrt(get_cov(freq, 'QQ', nside_out=nside_out))
     s_UU = np.sqrt(get_cov(freq, 'UU', nside_out=nside_out))
     s2_QU = get_cov(freq, 'QU', nside_out=nside_out)
+    rescal_atm = 0.8 * cli(get_rhits(freq, nside_out=nside_out))
+    nlev_I = get_nlev_ukamin(freq, 'II', thres=2)
+    nlev_P = np.sqrt(0.5 * (get_nlev_ukamin(freq, 'QQ', thres=2) ** 2 + get_nlev_ukamin(freq, 'UU', thres=2) ** 2 ))
+    print('noise levels for atm noise %.3f, %.3f'%(nlev_I, nlev_P))
     # Ignoring IQ and IU cov
     rdet_P = s_QQ ** 2 * s_UU ** 2 - s2_QU ** 2
     nz = rdet_P.nonzero()
 
 
     geom = Geom.get_healpix_geometry(nside_out).restrict(tht_min, tht_max, False, update_ringstart=False)
-    nthreads = int(os.environ.get('OMP_NUM_THREADS', cpu_count()))
+    nthreads = int(os.environ.get('OMP_NUM_THREADS', cpu_count(logical=False)))
 
     # ---- syn alm ----
-    cl_knee_I = np.ones(lmax_atm + 1, dtype=float) * (np.arange(1, lmax_atm + 2) / lknee_I) ** (-alpha_I)
-    cl_knee_P = np.ones(lmax_atm + 1, dtype=float) * (np.arange(1, lmax_atm + 2) / lknee_P) ** (-alpha_P)
+    # We set the knee'ed power to mach the white noise level at lknee
+    cl_knee_I = (nlev_I / 180 / 60 * np.pi) ** 2 * (np.arange(1, lmax_atm + 2) / lknee_I) ** (-alpha_I)
+    cl_knee_P = (nlev_P / 180 / 60 * np.pi) ** 2 * (np.arange(1, lmax_atm + 2) / lknee_P) ** (-alpha_P)
+    tim.close('one-off')
 
-    rng = default_rng(seed=seed)
-    tlm = _syn_alm(rng, cl_knee_I * _facknee)
-    eblm = np.zeros((2, Alm.getsize(lmax_atm, lmax_atm)), dtype=complex)
-    eblm[0] = _syn_alm(rng, (cl_knee_P * _facknee) * (np.arange(lmax_atm + 1) >= 2))
-    eblm[1] = _syn_alm(rng, (cl_knee_P * _facknee) * (np.arange(lmax_atm + 1) >= 2))
+    for idx in mcs:
+        fn = 'tqu_%04d'%int(idx)
+        if not cache.is_cached(fn):
+            tim.start('gen')
+            tlm = _syn_alm(rng, cl_knee_I * np.sqrt(_facknee))
+            eblm = np.zeros((2, Alm.getsize(lmax_atm, lmax_atm)), dtype=complex)
+            eblm[0] = _syn_alm(rng, (cl_knee_P * np.sqrt(_facknee)) * (np.arange(lmax_atm + 1) >= 2))
+            eblm[1] = _syn_alm(rng, (cl_knee_P * np.sqrt(_facknee)) * (np.arange(lmax_atm + 1) >= 2))
 
-    # ---- maps ----
-    tqumap = np.zeros((3, 12 * nside_out ** 2))
-    geom.synthesis(tlm, 0, lmax=lmax_atm, mmax=mmax, nthreads=nthreads, map=tqumap[0:1])
-    tqumap[0] *= (s_II / np.sqrt(s2_white_I))
-    tqumap[0, nz] += s_II[nz] * rng.standard_normal(nz[0].size) # add perfectly white noise
+            # ---- maps ----
+            tqumap = np.zeros((3, 12 * nside_out ** 2))
+            geom.synthesis(tlm, 0, lmax=lmax_atm, mmax=mmax, nthreads=nthreads, map=tqumap[0:1])
+            tqumap[0] *= rescal_atm
+            tqumap[0, nz] += s_II[nz] * rng.standard_normal(nz[0].size) # add white noise in pixel space, avoiding lmax issues
 
-    geom.synthesis(eblm, 2, lmax_atm, mmax, nthreads=nthreads, map=tqumap[1:])
-    tqumap[1] *= (s_QQ  / np.sqrt(s2_white_P))
-    tqumap[2] *= (s_UU  / np.sqrt(s2_white_P))
-    # Include QU covariance in Pol
-    xy = rng.standard_normal((2 , nz[0].size)) * _facwhite
-    tqumap[1, nz] += s_QQ[nz] * xy[0]
-    tqumap[2, nz] += (s2_QU[nz]  * xy[0] + np.sqrt(rdet_P[nz]) * xy[1]) /  s_QQ[nz]
-    tqumap[np.where(tqumap == 0.)] = hp.UNSEEN
-    return tqumap
-
-
-def get_sim_map(freq, lmax=5120, nside_out=2048, seed=None, _facknee=1., _facwhite=1.):
-
-    # ---- sim params and geom ----
-    mmax = lmax
-    lknee_I, alpha_I = get_lknee_a('T', freq)
-    lknee_P, alpha_P = get_lknee_a('P', freq)
-
-    cl_white = np.ones(lmax + 1, dtype=float)
-    s2_white_I = np.sum(cl_white * (2 * np.arange(lmax + 1) + 1)) / (4 * np.pi)
-    s2_white_P = np.sum(cl_white[2:] * (2 * np.arange(2, lmax + 1) + 1)) / (4 * np.pi) # ( EE + BB)
-
-    rescal_I = np.sqrt(get_cov(freq, 'II', nside_out=nside_out) / s2_white_I) # Such that white part matches Reijo depth
-    rescal_Q = np.sqrt(get_cov(freq, 'QQ', nside_out=nside_out) / s2_white_P)
-    rescal_U = np.sqrt(get_cov(freq, 'UU', nside_out=nside_out) / s2_white_P)
-
-    geom = Geom.get_healpix_geometry(nside_out).restrict(tht_min, tht_max, False, update_ringstart=False)
-    nthreads = int(os.environ.get('OMP_NUM_THREADS', cpu_count()))
-
-    # ---- syn alm ----
-    cl_knee_I = np.ones(lmax + 1, dtype=float)  * (np.arange(1, lmax + 2) / lknee_I) ** (-alpha_I) * _facknee
-    cl_knee_P = np.ones(lmax + 1, dtype=float)  * (np.arange(1, lmax + 2) / lknee_P) ** (-alpha_P) * _facknee
-
-    rng = default_rng(seed=seed)
-    tlm = _syn_alm(rng, cl_white * _facwhite + cl_knee_I * _facknee)
-    eblm = np.zeros((2, Alm.getsize(lmax, lmax)), dtype=complex)
-    eblm[0] = _syn_alm(rng, (cl_white * _facwhite + cl_knee_P * _facknee) * (np.arange(lmax + 1) >= 2))
-    eblm[1] = _syn_alm(rng, (cl_white * _facwhite + cl_knee_P * _facknee) * (np.arange(lmax + 1) >= 2))
-
-    # ---- maps ----
-    tqumap = np.full((3, 12 * nside_out ** 2), hp.UNSEEN)
-    geom.synthesis(tlm, 0, lmax=lmax, mmax=mmax, nthreads=nthreads, map=tqumap[0:1])
-    tqumap[0, np.where(rescal_I)] *= rescal_I[np.where(rescal_I)]
-
-    geom.synthesis(eblm, 2, lmax, mmax, nthreads=nthreads, map=tqumap[1:])
-    tqumap[1, np.where(rescal_Q)] *= rescal_Q[np.where(rescal_Q)]
-    tqumap[2, np.where(rescal_U)] *= rescal_U[np.where(rescal_U)]
-    return tqumap
+            geom.synthesis(eblm, 2, lmax_atm, mmax, nthreads=nthreads, map=tqumap[1:])
+            tqumap[1] *= rescal_atm
+            tqumap[2] *= rescal_atm
+            # Include QU covariance in Pol
+            if _facwhite > 0:
+                xy = rng.normal(scale=_facwhite, size=(2 , nz[0].size))  # add white noise in pixel space
+                tqumap[1, nz] += s_QQ[nz] * xy[0]
+                tqumap[2, nz] += (s2_QU[nz]  * xy[0] + np.sqrt(rdet_P[nz]) * xy[1]) /  s_QQ[nz]
+            tim.close('gen')
+            tim.start('unseening')
+            tqumap[np.where(tqumap == 0.)] = UNSEEN
+            tim.close('unseening')
+            print(tim)
+            cache.cache(fn, tqumap[:, npix: pix_min + npix])
+    return cache
 
 if __name__ == '__main__':
     for XX in ['II', 'QQ', 'UU']:
--- 20240124_chwide-noise/chwide_maps.py
+++ 20240124_chwide-noise/chwide_maps.py
@@ -1,24 +1,33 @@
 import numpy as np
-import healpy as hp
+UNSEEN = -1.6375e+30
+
+try:
+    from healpy import UNSEEN as hp_UNSEEN
+    if UNSEEN != hp_UNSEEN:
+        print("using %.5e for UNSEEN pixels" % UNSEEN)
+except ImportError:
+    UNSEEN = -1.6375e+30
+    hp_UNSEEN = None
+    print("using %.5e for UNSEEN pixels"%UNSEEN)
 
 tht_min, tht_max = (64 / 180 * np.pi, 155. / 180 * np.pi)
 pix_min, npix = 14135296, 33839732
-path = '' #FIXME
+path = '/global/cfs/cdirs/cmbs4xlb/v1/noise/chwide/tqu_%04d.npy'
 
 class noise:
     @staticmethod
     def get_sim_tmap(freq, idx):
-        m = np.full( (12 * 2048 ** 2), hp.UNSEEN)
+        m = np.full( (12 * 2048 ** 2), UNSEEN)
         m[pix_min: pix_min + npix] = np.load(path%(freq, idx))[0]
         return m
     @staticmethod
     def get_sim_qumap(freq, idx):
-        m = np.full((2, 12 * 2048 ** 2), hp.UNSEEN)
+        m = np.full((2, 12 * 2048 ** 2), UNSEEN)
         m[:, pix_min:pix_min + npix] = np.load(path%(freq, idx))[1:]
         return m
     @staticmethod
     def get_sim_tqumap(freq, idx):
-        m = np.full((3, 12 * 2048 ** 2), hp.UNSEEN)
+        m = np.full((3, 12 * 2048 ** 2), UNSEEN)
         m[:, pix_min:pix_min + npix] = np.load(path%(freq, idx))
         return m
 
